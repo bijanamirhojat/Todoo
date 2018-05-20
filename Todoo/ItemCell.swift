e239dd03024d0e48ef5bd3f464cfa3027df857ba
Bijan Amir-Hojat <amirhojat@mediamarkt.nl>
2018-05-20 22:33:58 +0200

    Table is main view

---
+++ Todoo/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  Todoo
+//
+//  Created by Bijan Amir-Hojat on 20-05-18.
+//  Copyright © 2018 bCreate. All rights reserved.
+//
+
+import Foundation
+
+class ItemCell {
+    
+    var title: String = ""
+    var catID: Int = 0
+    var completed: Bool = false
+    
+}
